3e2bf36c447b1ea37958842c60ee13ac117f71ce
Chandan Tiwary <60096802+ctiwary36@users.noreply.github.com>
2021-06-12 09:18:00 +0530

    Update Helloworld.java

--- JavaPractice/Helloworld.java
+++ JavaPractice/Helloworld.java
@@ -2,6 +2,7 @@ package JavaPractice;
 
 public class Helloworld {
 public static void main(String[] args) {
-	System.out.println("Hello World!!");
+	System.out.println("Hello World!! Change");
+	System.out.println("change1 made");
 }	
 }
